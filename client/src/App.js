d6413d264ca5db56659b8cac2e54072ff35b8b7b
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-07 18:06:35 -0400

    added route for search

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,16 @@
 import React from "react";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
+import Search from "./pages/Search";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// The app will not render correctly until you setup a Route component.
-// Refer to the Basic Example documentation if you need to.
-// (https://reacttraining.com/react-router/web/example/basic)
+
 function App() {
 	return (
 		<Router>
 			<Nav />
 			<Switch>
-				<Route exact path="/" component={Books} />
-				<Route exact path="/books" component={Books} />
-				<Route exact path="/books/:id" component={Detail} />
+				<Route exact path="/" component={Search} />
+				<Route exact path="/search" component={Search} />
 				<Route path="*" component={NoMatch} />
 			</Switch>
 		</Router>
