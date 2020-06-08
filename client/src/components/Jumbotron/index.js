706d8a0a392d35708d9442bbc59165f7cec27a42
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-07 20:57:14 -0400

    moved header

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -6,7 +6,8 @@ function Jumbotron({ children }) {
       style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
       className="jumbotron"
     >
-      {children}
+        <h1>(React) Google Books Search</h1>
+				<p>Search for and Save Books of Interest</p>
     </div>
   );
 }
